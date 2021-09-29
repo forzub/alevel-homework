7027acd17f1ba8cd5542c07e0e075b0aad5264f6
forzub <forzub@gmail.com>
2021-09-29 10:48:51 +0300

    home-work js4

---
+++ 2021-09-28-js-004/index.js
@@ -0,0 +1,112 @@
+{
+console.log("task - 1");
+console.log(`1. Написать функцию bindFunc, которая принимает в себя 2 + аргументов 
+(Точно должна принять 2 аргумента, а дальше сколько угодно).
+1 аргумент - какая-то функция
+2 аргумент - значение контекста
+3 + ... аргументы - любое кол-во аргументов
+Эта функция, должна устанавливать контекст для функции, которая в первом аргументе, 
+и возвращать эту функцию с новым контекстом.
+Сам контекст, который мы хотим установить, находиться во втором аргументе`);
+
+function bindFunc(func,context,...arg){
+    return func.bind(context);
+}
+
+}
+
+// ----------------------------------------------------------------------- 
+{
+console.log("task - 2");
+console.log(`2. Написать функцию, которая не принимает никаких аргументов. 
+В теле функции написать логику для нахождения суммы значений 
+любого количества ключей (значения ключей должны быть больше нуля) 
+из переданного контекста.
+Обращаться к objectA напрямую нельзя`);
+
+const objectA = {
+    a: 1,
+    b: 2,
+    c: 3,
+   }
+
+   const func = function() {
+       let sum = 0;
+       let keys_arr = [...Object.keys(this)];
+       let response =`BAD ARGUMENTS`;
+
+
+       if(keys_arr.length){
+            for(let i of keys_arr){
+                sum += this[i];
+            }
+        }
+        response = `sum = ${sum}`; 
+        return console.log(response);
+   }
+
+let contextsum = func.bind(objectA)();
+}
+  
+// -----------------------------------------------------------------------
+{
+console.log("task - 3");
+console.log(`3. Написать функцию, которая возвращает новый массив, 
+в котором должны быть только четные числа, которые больше двуx и меньше 10. 
+Новый массив будет состоять из значений ключа values из контекста, 
+если такого ключа нет, то выводим сообщение "Не найдено".
+Обращаться к valObject0 напрямую нельзя :)
+Если хотите использовать map, то внутри map this всегда равен глобальному объекту. 
+Чтобы это поменять передаем нужное значение this во второй аргумент map -
+arr.map(() => {}, this);`);
+
+const valObject0 = { 
+                    values: [1, '2', 4, 8, '8',  3, 10, null, false],
+                    };
+
+    function getNewArray() {
+        let response = 'Не найдено';
+ 
+        for(let key of Object.keys(this)){
+            if(key === 'values'){
+
+                response = this[key].map((curval)=>{
+                    if(typeof curval !== 'number') return;
+                    if((curval < 3) || (curval > 9)) return;
+                    if((curval % 2) === 0) return curval;
+                });
+                response = response.filter(y => y != undefined);
+            }
+        }       
+        return response;
+       };
+
+let sort_arr = getNewArray.bind(valObject0)();
+console.log(sort_arr);
+}
+
+// -----------------------------------------------------------------------
+{
+    console.log("task - скобки");
+    let str = "((()((())()()((()()()(()())()()()()())((())))(((()()()()())()()()()))(()))";
+    let round_brackets = 0;
+    let str1 = "";
+    let str2 = "";
+
+    for(let i = 0; i < str.length; i++){
+        if(str[i] === "("){ 
+            round_brackets++;
+            str1 += "(";
+        }
+        
+        if(str[i] === ")"){
+            round_brackets--;
+            str2 += ")";
+        } 
+    }
+
+    console.log(`round_brackets = ${round_brackets}`);
+    console.log(`( = ${str1}`);
+    console.log(`) = ${str2}`);
+
+}    
